5f348b39bb284486406108f54068e1b2e7c3daba
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-06-06 23:28:53 +0530

    simple_interest

--- simple_interest.dart
+++ simple_interest.dart
@@ -1,13 +1,3 @@
-// import 'dart:io';
-//
-// main(){
-//   int n1, n2, sum;
-//   print("ente the first number: ");
-//   n1 = int.parse(stdin.readLineSync()!);
-//   print("enter the second number: ");
-//   n2 = int.parse(stdin.readLineSync()!);
-//   print(n1 + n2);
-// }
 import 'dart:io';
 
 main() {
