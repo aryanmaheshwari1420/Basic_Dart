e79ea0d0df955966dcff4fef67bd099d4f667f3f
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-06-06 22:22:43 +0530

    one more example of conversion

--- type_conversion.dart
+++ type_conversion.dart
@@ -1,9 +1,13 @@
- void main() {
-  
-
-bool value = true;
+void main() {
+  bool value = true;
   print("Before Conversion => ${value.runtimeType}");
   var convertedVal = value.toString();
   print("After Conversion==> ${convertedVal.runtimeType}");
   print(convertedVal);
+
+  int val = 10;
+  print("before changing datatype => ${val.runtimeType}");
+  var converted_val = val.toDouble();
+  print("after  changing datatype => ${converted_val.runtimeType}");
+
 }
